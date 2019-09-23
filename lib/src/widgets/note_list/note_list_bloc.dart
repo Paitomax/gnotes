4a6859ed728ac5992305a199704f7637be5ed56a
Guilherme Nascimento <paitomax@gmail.com>
2019-09-22 22:58:51 -0300

    added note list initial commit

---
+++ lib/src/widgets/note_list/note_list_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:gnotes/src/widgets/note_list/note_list_state.dart';
+
+class NoteListWidgetBloc extends Bloc<NoteListWidgetState, NoteListWidgetState>{
+  @override
+  // TODO: implement initialState
+  NoteListWidgetState get initialState => null;
+
+  @override
+  Stream<NoteListWidgetState> mapEventToState(NoteListWidgetState event) {
+    // TODO: implement mapEventToState
+    return null;
+  }
+
+}
---
+++ lib/src/widgets/note_list/note_list_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class NoteListWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
+
+class NoteListState extends State<NoteListWidget>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
