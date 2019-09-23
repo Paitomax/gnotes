f0ce04ca4034fc33d29ba3fe02ac730b728c2960
Guilherme Nascimento <paitomax@gmail.com>
2019-09-22 23:30:14 -0300

    note list created

---
+++ lib/src/widgets/note_list/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel {
+  int id;
+  String title;
+  String body;
+  DateTime dateTime;
+
+  NoteModel(this.id, this.title, this.body, {this.dateTime});
+}
--- lib/src/widgets/note_list/note_list_widget.dart
+++ lib/src/widgets/note_list/note_list_widget.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class NoteListWidget extends StatefulWidget{
+import 'models/note_model.dart';
+
+class NoteListWidget extends StatefulWidget {
+  List<NoteModel> items;
+
+  NoteListWidget(this.items);
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
+    return NoteListState();
   }
-
 }
 
-class NoteListState extends State<NoteListWidget>{
+class NoteListState extends State<NoteListWidget> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return ListView.separated(
+      itemCount: widget.items.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  widget.items[index].title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                child: Text(
+                  widget.items[index].title,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+                ),
+              )
+            ],
+          ),
+          onTap: () => Scaffold.of(context)
+              .showSnackBar(SnackBar(content: Text("Cliquei no item $index"))),
+        );
+      },
+    );
   }
-
 }
