0674042c125eda17f76a55f67d9380c1baf91e01
Guilherme Nascimento <paitomax@gmail.com>
2019-10-05 01:42:23 -0300

    test commented

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gnotes/src/add_note/add_note_screen.dart';
 import 'package:gnotes/src/widgets/note_list/models/note_model.dart';
 import 'package:gnotes/src/widgets/note_list/note_list_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +24,35 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  goToAddNoteScreen(){
-    //Navigator.push(context, route)
+  goToAddNoteScreen() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return AddNoteScreen();
+    }));
   }
 
   List<NoteModel> createMock() {
     List<NoteModel> list = List<NoteModel>();
     list.addAll([
-      NoteModel(1, 'Jogar', 'Hoje preciso jogar até o amanhecer'),
-      NoteModel(2, 'Tarefa de inglês',
-          'O baguiu vai ser louco se eu não fizer essas tarefas marotas.'),
-      NoteModel(3, 'Aniversario da Line',
-          'NÃO POSSO ESQUECER DE PARABENIZAR MEU AMORZÃO HOJE'),
-      NoteModel(4, 'Ihuu',
-          'Cansei de escrever os coiso tudo, quero ver logo essa tela'),
+      NoteModel(
+        'Jogar',
+        'Hoje preciso jogar até o amanhecer',
+        id: 1,
+      ),
+      NoteModel(
+        'Tarefa de inglês',
+        'O baguiu vai ser louco se eu não fizer essas tarefas marotas.',
+        id: 1,
+      ),
+      NoteModel(
+        'Aniversario da Line',
+        'NÃO POSSO ESQUECER DE PARABENIZAR MEU AMORZÃO HOJE',
+        id: 1,
+      ),
+      NoteModel(
+        'Ihuu',
+        'Cansei de escrever os coiso tudo, quero ver logo essa tela',
+        id: 1,
+      ),
     ]);
     return list;
   }
