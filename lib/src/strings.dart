c1c322c7e54058e4500ffaa4d5168013a8e575a8
Guilherme Nascimento <paitomax@gmail.com>
2021-02-11 07:28:53 -0300

    Add app strings

---
+++ lib/src/strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  static const String appName = 'Gnotes';
+  static const String title = 'Título';
+  static const String description = 'Descrição';
+  static const String addNew = 'Adicionar novo';
+  static const String delete = 'Deletar';
+  static const String exit = 'Sair';
+  static const String newNote = 'Nova Nota';
+  static const String editNote = 'Editar Nota';
+  static const String signInWithGoogle = 'Entrar com Google';
+}
